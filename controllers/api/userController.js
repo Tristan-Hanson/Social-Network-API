7e848d2702856ff7c8804bb4c3a6d5d8afc9932e
Tristan Hanson <thanson1998@live.com>
2022-11-05 22:38:43 -0500

    controller routes for user are done and working

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+//get all users
 router.get('/', async (req, res) =>{
   try{
     const profile = await User.find()
@@ -9,5 +11,51 @@ router.get('/', async (req, res) =>{
     res.json(err).status(400)
   }
 })
+
+//get one user
+router.get("/:id", async(req,res) =>{
+  try{
+      const singleUser = await User.findById(req.params.id);
+      res.json(singleUser).status(200);
+  }catch(err){
+      res.json(err).status(400);
+  }
+});
+
+//new user route
+router.post("/", async(req,res) =>{
+  try{
+      const newUser = await User.create({...req.body})
+      console.log(newUser)
+      res.json(newUser).status(200);
+  }catch(err){
+      res.json(err).status(400);
+  }
+});
+
+
+//update user route
+router.put("/:id", async(req,res)=>{
+  try{
+      const userUpdate = await User.findByIdAndUpdate(
+          req.params.id,
+          req.body,
+          {new: true}
+      )
+      res.json(userUpdate).status(200);
+  }catch(err){
+      res.json(err).status(400);
+  }
+});
+
+//Delete user route
+router.delete("/:id", async(req,res)=>{
+  try{
+      const deletedUser = await User.findByIdAndDelete(req.params.id);
+      res.json(deletedUser).status(200);
+  }catch(error){
+      res.json(err).status(400);
+  }
+});
 
 module.exports = router;
